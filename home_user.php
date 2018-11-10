9c867fc555b9b1e60fd25178b1fde7280c91fb62
reshmalr033 <reshmalr033@gmail.com>
2018-11-10 09:55:04 +0530

    home page of user
    home page of user

---
+++ home_user.php
@@ -0,0 +1,146 @@
+
+<?php
+include 'menu_user.php';
+?>
+<?php
+
+if($z=$_SESSION['username'])
+{
+    
+}
+ else {
+    header("location:index.php");    
+}
+?>
+				<!-- start-search-->
+			<div class="clearfix"> </div>
+        </div>
+			 	<div class="banner">
+						<div class="callbacks_container">
+						<ul class="rslides callbacks callbacks1" id="slider4">
+							<li class="" style="display: block; float: none; position: absolute; opacity: 0; z-index: 1; transition: opacity 500ms ease-in-out;">
+								<div class="banner-info">
+								   <h3>Find Your Dream Car</h3>
+								   <p>Find used cars on <span>recent cars</span> page</p>
+								</div>
+							</li>
+							<li class="callbacks1_on" style="display: block; float: left; position: relative; opacity: 1; z-index: 2; transition: opacity 500ms ease-in-out;">
+								<div class="banner-info">
+								  <h3>We Give Best Service</h3>
+								  <p>Find used cars on <span>recent cars</span> page</p>
+								</div>	
+							</li>
+							<li class="" style="display: block; float: none; position: absolute; opacity: 0; z-index: 1; transition: opacity 500ms ease-in-out;">
+								<div class="banner-info">
+								   <h3>Find Your Dream Car</h3>
+								    <p>Find used cars on <span>recent cars</span> page</p>
+								</div>
+							</li>
+							<li class="" style="display: block; float: none; position: absolute; opacity: 0; z-index: 1; transition: opacity 500ms ease-in-out;">
+								<div class="banner-info">
+								  <h3>We Give Best Service</h3>
+								  <p>Find used cars on <span>recent cars</span> page</p>
+								</div>	
+							</li>
+							<li class="" style="display: block; float: none; position: absolute; opacity: 0; z-index: 1; transition: opacity 500ms ease-in-out;">
+								<div class="banner-info">
+								  <h3>Lorem Ipsum is simply</h3>
+								  <p>Find used cars on <span>recent cars</span> page</p>
+								</div>	
+							</li>
+						</ul>
+						</div>
+						<!--banner-Slider-->
+						<script src="js/responsiveslides.min.js"></script>
+						
+			</div>
+		</div>
+	</div>
+	<!--start-banner-bottom-->
+	<!-- welcome-bottom -->
+	<div class="welcome-bottom">
+		<div class="container">
+			<div class="welcome-bottom-banner">
+				<h3 class="tittle">Special Services</h3>
+				<div class="welcome-bottom-grids">
+					<div class="welcome-bottom-grid-left">
+						<div class="welcome-bottom-grid-left-g">
+							<img src="images/7.jpg" alt=" " class="img-responsive" />
+							<div class="welcome-bottom-grid-left-gpos">
+								<h4>Pellentesque mollis</h4>
+								<p>Nulla et finibus libero. Suspendisse vitae ex facilisis, ultricies est sed, porta ante. Vivamus tristique luctus lorem, eget dignissim lacus sodales tristique.</p>
+								<div class="more">
+									<a href="#" class="hvr-bounce-to-bottom m1">More Info...</a>
+								</div>
+							</div>
+						</div>
+						<div class="welcome-bottom-grid-left-g">
+							<img src="images/8.jpg" alt=" " class="img-responsive" />
+							<div class="welcome-bottom-grid-left-gpos1">
+								<h4>Pellentesque mollis</h4>
+								<p>Nulla et finibus libero. Suspendisse vitae ex facilisis, ultricies est sed, porta ante. Vivamus tristique luctus lorem, eget dignissim lacus sodales tristique.</p>
+								<div class="more">
+									<a href="#" class="hvr-bounce-to-bottom m1">More Info...</a>
+								</div>
+							</div>
+						</div>
+						<div class="car-text">
+							<h5>Lorem Ipsum is simply</h5>
+							<p>Dolor amet At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias </p>
+						
+						</div>
+					</div>
+					<div class="welcome-bottom-grid-right">
+						<div class="car-text">
+						<h5>Lorem Ipsum is simply</h5>
+							<p>Dolor amet At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias </p>
+							
+						</div>
+						<div class="welcome-bottom-grid-left-g">
+							<img src="images/9.jpg" alt=" " class="img-responsive" />
+							<div class="welcome-bottom-grid-left-gpos">
+								<h4>Pellentesque mollis</h4>
+								<p>Nulla et finibus libero. Suspendisse vitae ex facilisis, ultricies est sed, porta ante. Vivamus tristique luctus lorem, eget dignissim lacus sodales tristique.</p>
+								<div class="more">
+									<a href="#" class="hvr-bounce-to-bottom m1">More Info...</a>
+								</div>
+							</div>
+						</div>
+						<div class="welcome-bottom-grid-left-g">
+							<img src="images/10.jpg" alt=" " class="img-responsive" />
+							<div class="welcome-bottom-grid-left-gpos1">
+								<h4>Pellentesque mollis</h4>
+								<p>Nulla et finibus libero. Suspendisse vitae ex facilisis, ultricies est sed, porta ante. Vivamus tristique luctus lorem, eget dignissim lacus sodales tristique.</p>
+								<div class="more">
+									<a href="#" class="hvr-bounce-to-bottom m1">More Info...</a>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="clearfix"> </div>
+				</div>
+			</div>
+		</div>
+	</div>
+<!-- //welcome-bottom -->
+
+<!--start-video-->
+    
+		<!--//news-->
+	<div class="car-bottom">
+		  <div class="container">
+			  <div class="col-md-6 car-bottom-right">
+					<h3>The key maintenance</h3>
+					<h4>for your safety <span> on road</span></h4>
+					<p>Lorem ipsum dolor sit amet,Ex ea commodo consequat Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan.</p>
+					<div class="morec1">
+					  <a href="#" class="hvr-bounce-to-top">More Info...</a>
+					</div>
+				</div>
+					<div class="clearfix"></div>
+		  </div>
+		</div>
+
+		<?php
+                        include 'footer.php';
+                        ?>
